9418e0d8263b43445a2855e0cf323d560a656ed9
MmaneoKhoaele <122862720+MmaneoKhoaele@users.noreply.github.com>
2024-04-07 01:05:56 +0200

    finalised everything

--- src/index.js
+++ src/index.js
@@ -18,7 +18,6 @@ function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
-  searchCity(Alberton);
 
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
